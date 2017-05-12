657503a0d391390529719e5e0e396cd8461b0f5d
Mikhail Epifanov <m.p.epifanov@student.tudelft.nl>
2017-05-12 17:12:23 +0200

    Completion of the First and Second assignment, Build Gateway, Forge, Cannons and pylons when needed.

--- src/ProtossGateway/ProtossGateway.pl
+++ src/ProtossGateway/ProtossGateway.pl
@@ -1,8 +1,9 @@
-:-dynamic 	resources/4, % to keep trakc of resources
-						friendly/3,		% Friendly units
-						builder/0,		% This drone is a builder
-						gasCollector/0,		% This drone is a gasCollector
-						self/2,			% our own ID from the self/5 percept
-						mineralField/2, 	% mineralField IDs and amounts from the mineralField/5 percept
-						workerActivity/1,	% our own activity state from the workerActivity/1 percept
-						gathered/1.		% used as a goal to gather mineralFields
+:-dynamic 	
+	self/2,			% our own ID from the self/5 percept
+	friendly/3,		% Friendly units
+	queueSize/1,		% The queueSize of the building
+	builder/0,		% This drone is a builder
+	gasCollector/0,		% This drone is a gasCollector
+	mineralField/2, 	% mineralField IDs and amounts from the mineralField/5 percept
+	workerActivity/1,	% our own activity state from the workerActivity/1 percept
+	gathered/1.		% used as a goal to gather mineralFields
