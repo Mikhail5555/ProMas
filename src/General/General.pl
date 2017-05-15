2df1d35a9949663ee4bf75776792886dc7ba3567
Hendrik Janse <hendrikjanse@live.com>
2017-05-15 07:43:58 +0200

    added the constuctionsite defense predicate

--- src/General/General.pl
+++ src/General/General.pl
@@ -11,9 +11,35 @@
  
  % Goals
  	target/2.		% Target of things you want to build
+ % Constructionsite
+	constructionSite/3.
  
 
 canBuild(Type) :- cost(Type, MCost, GCost, SCost), resources(CurrentMinerals, CurrentGas, CurrentSupply, MaxSupply), CurrentMinerals >= MCost, CurrentGas >= GCost, (CurrentSupply + SCost) =< MaxSupply.
+
+
+%the calculation of distance between two coordinates.
+distance(X1,Y1,X2,Y2, Distance) :-
+    abs(X1-X2, X3),
+    abs(Y1-Y2, Y3),
+     sqrt(X3*X3 + Y3*Y3, Distance).
+
+
+%returns the construction sites which have atleast 5 distance from the chokepoint.
+withinChoke(ChokeX, ChokeY, X, Y) :- 
+    constructionSite(X, Y, _),
+    distance(X, Y, ChokeX,ChokeY, Distance),
+    Distance < 5.
+
+% returns the construction sites near a chokepoint which are near the constructionSites but on the base side. 
+defenseSpot(ChokeX, ChokeY, BaseX, BaseY, X, Y) :-
+    constructionSite(X,Y, _),
+    withinChoke(ChokeX, ChokeY, X, Y),
+    distance(ChokeX, ChokeY, BaseX, BaseY, BaseDistance),
+    distance(BaseX, BaseY, X, Y, SiteDistance),
+    SiteDistance < BaseDistance.
+
+
 
 % cost(UnitType, Mineral, Gas, Supply) of units
 
