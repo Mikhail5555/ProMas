626ea1b792aff47ee219a65b89a7afee70c5784f
Kevin den Boon <K.denboon@student.tudelft.nl>
2017-05-19 14:47:49 +0200

    Now agents will be launched when creating cybernetics core and stargate.

--- src/ProtossGateway/ProtossGateway.pl
+++ src/ProtossGateway/ProtossGateway.pl
@@ -1,9 +1,4 @@
 :-dynamic 	
 	self/2,			% our own ID from the self/5 percept
 	friendly/3,		% Friendly units
-	queueSize/1,		% The queueSize of the building
-	builder/0,		% This drone is a builder
-	gasCollector/0,		% This drone is a gasCollector
-	mineralField/2, 	% mineralField IDs and amounts from the mineralField/5 percept
-	workerActivity/1,	% our own activity state from the workerActivity/1 percept
-	gathered/1.		% used as a goal to gather mineralFields
+	queueSize/1.		% The queueSize of the building
